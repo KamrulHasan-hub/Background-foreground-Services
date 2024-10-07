475a07352a8bd147dc3904cd48252d1f2d4568d2
kamrul <kamrulhasankganj@gmail.com>
2024-10-07 12:27:54 +0600

    update

---
+++ app/src/main/java/com/example/backgroundandforegroundservices/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.backgroundandforegroundservices
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import android.Manifest
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.POST_NOTIFICATIONS),200)
+
+
+
+        findViewById<Button>(R.id.BTN1).setOnClickListener {
+
+            val intent= Intent(this,MyService::class.java)
+            intent.action=Actions.START.toString()
+            startService(intent)
+
+        }
+
+        findViewById<Button>(R.id.BTN2).setOnClickListener {
+
+            val intent= Intent(this,MyService::class.java)
+            intent.action=Actions.STOP.toString()
+            startService(intent)
+
+        }
+
+    }
+}
